e0625e1c37452d46f08366c44783a48c5b5ffbb1
Mandoong <111546154+mandoong@users.noreply.github.com>
2024-03-12 22:53:15 +0900

    project init

---
+++ src/components/basic/svg.js
@@ -0,0 +1,12 @@
+export default function BasicSvg({ src, width, height, color }) {
+  return (
+    <div
+      style={{
+        maskImage: src,
+        width,
+        height,
+        color,
+      }}
+    ></div>
+  );
+}
---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import BasicSvg from "../../components/basic/svg";
+
+export default function Home() {
+  return (
+    <>
+      <div>home page</div>
+      <BasicSvg
+        src="/logo192.png"
+        width={"20px"}
+        height={"30px"}
+        color={"red"}
+      />
+    </>
+  );
+}

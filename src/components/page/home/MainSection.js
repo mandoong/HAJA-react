dd352f8422381ffb3b51efe5ecb1aca548502162
Mandoong <111546154+mandoong@users.noreply.github.com>
2024-03-31 20:50:02 +0900

    feat: main section add animation

--- src/components/page/home/MainSection.js
+++ src/components/page/home/MainSection.js
@@ -1,8 +1,28 @@
+import { useRef, useState } from "react";
 import BasicUserProfile from "../../basic/user/UserProfile";
 import SearchBar from "./SearchBar";
 import "./MainSectionCss.css";
 
 export default function HomeMainSection() {
+  const imgSlider = useRef();
+  const [curImage, setCurImage] = useState();
+  const imgs = [
+    {
+      url: "https://assets.awwwards.com/awards/sites_of_the_day/2024/03/outpost-cover.jpg",
+      desc: "By",
+      title: "Outpost",
+    },
+    {
+      url: "https://assets.awwwards.com/awards/sites_of_the_day/2024/03/g1-13.jpg",
+      desc: "Site Name",
+      title: "TWICE",
+    },
+  ];
+
+  imgSlider.current = setInterval(() => {
+    setCurImage();
+  }, 3000);
+
   return (
     <div className="bg-[#e9e9e9] w-full py-4 px-10">
       <SearchBar />
@@ -10,7 +30,7 @@ export default function HomeMainSection() {
         <div className="w-full font-roboto text-center text-[18vw] whitespace-nowrap">
           SITE OF THE DAY
         </div>
-        <div className="w-full overflow-hidden max-w-[1200px] box-border">
+        <div className="w-full overflow-hidden box-border">
           <div className="flex text-[20px] w-full items-center move ease-linear">
             <div className="text-nowrap min-w-full">
               <strong>Outpost</strong>— Site of the Day - <strong>7.32</strong>{" "}
@@ -25,13 +45,37 @@ export default function HomeMainSection() {
       </div>
       <div className="w-full h-96 bg-slate-100 rounded-xl overflow-hidden relative">
         <img
-          src="https://assets.awwwards.com/awards/sites_of_the_day/2024/03/outpost-cover.jpg"
+          src={imgs[0].url}
           className="w-full h-full object-cover"
           alt="main"
         />
+
         <div className="absolute bottom-4 left-4 font-over_b text-white">
-          <div className="text-[30px] font-over">By</div>
-          <div className="text-[60px]">Outpost</div>
+          <div className="text-[30px] h-[45px] font-over overflow-hidden ">
+            <div className="h-full slider">
+              {imgs.map((e) => {
+                return (
+                  <div className="h-full " key={e.desc}>
+                    {e.desc}
+                  </div>
+                );
+              })}
+              <div className="h-full">{imgs[0].desc}</div>
+            </div>
+          </div>
+
+          <div className="text-[60px] h-[80px] font-over_b overflow-hidden">
+            <div className="h-full slider">
+              {imgs.map((e) => {
+                return (
+                  <div className="h-full" key={e.title}>
+                    {e.title}
+                  </div>
+                );
+              })}
+              <div className="h-full">{imgs[0].title}</div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="w-full h-32 flex justify-center items-center gap-12">

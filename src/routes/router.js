e9a82c7a2947edcdbfab7e737c66a0329f8f8866
Mandoong <111546154+mandoong@users.noreply.github.com>
2024-04-10 08:39:30 +0900

    💚add: basic tag (web hook test)

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,4 @@
 import { Outlet, createBrowserRouter } from "react-router-dom";
-import About from "../pages/about/Index";
 import LayoutDefault from "../layouts/default";
 import AboutId from "../pages/about/[id]";
 import ProjectList from "../pages/projectList/index";
@@ -17,10 +16,6 @@ export default createBrowserRouter([
       {
         path: "/",
         element: <Home />,
-      },
-      {
-        path: "/about",
-        element: <About />,
       },
       {
         path: "/about/:id",

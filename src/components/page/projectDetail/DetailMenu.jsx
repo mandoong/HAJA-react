5b267151cfdd9d2d9381371edfa66f0cdefa8004
hanokfairy <hanokfairy@gmail.com>
2024-06-08 19:36:41 +0900

    :sparkles:feat:project detail added

--- src/components/page/projectDetail/DetailMenu.jsx
+++ src/components/page/projectDetail/DetailMenu.jsx
@@ -1,30 +1,34 @@
 import React, { useState } from "react";
 import TabMenu from "../../basic/menu/TapMenu";
+import DetailMain from "./DetailMainSection";
 
 export default function DetailMenu() {
   const tabItems = [
-    { label: "정보", content: "탭 1 내용" },
-    { label: "퀘스트", content: "퀘스트" },
-    { label: "질문", content: "질문" },
-    { label: "할일", content: "할일" },
-    { label: "홍보", content: "홍보" },
-    { label: "관리", content: "관리" },
+    { id: "info", label: "정보", content: <DetailMain /> },
+    { id: "question", label: "질문", content: "질문" },
+    { id: "todo", label: "할일", content: "할일" },
+    { id: "marketin", label: "홍보", content: "홍보" },
+    { id: "manage", label: "관리", content: "관리" },
   ];
 
-  const [activeTab, setActiveTab] = useState(0);
+  const [activeTab, setActiveTab] = useState("info");
 
-  const handleTabClick = (index) => {
-    setActiveTab(index);
+  const handleTabClick = (id) => {
+    setActiveTab(id);
   };
 
+  const activeContent = tabItems.find((tab) => tab.id === activeTab)?.content;
+
   return (
-    <div className="w-full mx-auto">
-      <TabMenu
-        tabItems={tabItems}
-        activeTab={activeTab}
-        onTabClick={handleTabClick}
-      />
-      <div className="p-4">{tabItems[activeTab].content}</div>
+    <div>
+      <div className="w-full mx-auto sticky top-0 bg-[#f8f8f8]">
+        <TabMenu
+          tabItems={tabItems}
+          activeTab={activeTab}
+          onTabClick={handleTabClick}
+        />
+      </div>
+      <div className="p-4">{activeContent}</div>
     </div>
   );
 }

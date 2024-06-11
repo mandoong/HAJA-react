4d5a62636afc45ad2701e560dda0d233f739cc7d
solightnsalt <soldakm@gmail.com>
2024-06-11 14:43:53 +0900

    🐛fix:lounge/list card 경로 수정

--- src/pages/lounge/List.jsx
+++ src/pages/lounge/List.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import LoungeCard from "../../components/page/lounge/Card";
+import LoungeCard from "../../components/page/lounge/card";
 
 export default function LoungeList({ posts, lastPostRef }) {
   const [currentTap, setCurrentTap] = useState("all");
